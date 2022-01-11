6fbd076d9a0e36a6b35346527342a767aa910956
Matthew <matt.cavallo56@gmail.com>
2022-01-11 13:31:40 -0600

    Add project files.

---
+++ NetCoreMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMVC.Models;
+
+namespace NetCoreMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new HelloWorldModel 
+            { 
+                HelloWorldModelString = "Hello World Model String Value" 
+            });
+        }
+    }
+}
---
+++ NetCoreMVC/Models/HelloWorldModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVC.Models
+{
+    public class HelloWorldModel
+    {
+        public string HelloWorldModelString { get; set; }
+    }
+}
